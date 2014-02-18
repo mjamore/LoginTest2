d83f425eb276cf3e0c92775539fc24385db2cd7a
Michael Amore <michael.amore@yahoo.com>
2014-02-17 18:25:50 -0600

    Input field coloring and error messages on blur.

--- includes/js/index.js
+++ includes/js/index.js
@@ -1,3 +1,163 @@
-function test() {
-	return "Michael";
-};
+function isEmailValid(email)
+{
+	var emailRegEx = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	var testAgainstRegEx = emailRegEx.test(email);
+	if( testAgainstRegEx )
+	{
+		console.log("email is valid");
+		return true;
+	}
+	else
+	{
+		console.log("email is NOT valid");
+		return false;
+	}
+}
+
+function isPWValid(pw)
+{
+	if( pw.length >= 8 )
+	{
+		// has a number && has at least 1 capital letter
+		var regEx = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/;
+		var abcd = regEx.test(pw);
+		if( abcd )
+		{
+			return true;
+		}
+		else
+		{
+			return false
+		}
+	}
+	else
+	{
+		return false;
+	}
+}
+
+function arePWsEqual(pw1, pw2)
+{
+	if( pw1 === pw2 )
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+
+
+function inputIsValid(input)
+{
+	$(this).removeClass("invalid").addClass("valid");
+	$(this).next().slideUp();
+}
+
+function inputIsInvalid(input)
+{
+	$(this).removeClass("valid").addClass("invalid");
+	$(this).next().slideDown();
+}
+
+
+
+(function()
+{
+	// login email
+	$('#email-login').on('blur', function() {
+		if( isEmailValid(this.value) )
+		{
+			inputIsValid.call(this);
+		}
+		else
+		{
+			inputIsInvalid.call(this);
+		}
+	});
+
+	$('#email-login').on('focus', function() {
+		$(this).removeClass("invalid");
+	});
+
+
+	// login pw
+	$('#pw-login').on('blur', function() {
+		if( isPWValid(this.value) )
+		{
+			inputIsValid.call(this);
+		}
+		else
+		{
+			inputIsInvalid.call(this);
+		}
+	});
+
+	$('#pw-login').on('focus', function() {
+		$(this).removeClass("invalid");
+	});
+
+
+
+
+	// register email
+	$('#email-register').on('blur', function() {
+		if( isEmailValid(this.value) )
+		{
+			inputIsValid.call(this);
+		}
+		else
+		{
+			inputIsInvalid.call(this);
+		}
+	});
+
+	$('#email-register').on('focus', function() {
+		$(this).removeClass("invalid");
+	});
+
+
+	// register pw
+	$('#pw1-register').on('blur', function() {
+		if( isPWValid(this.value) )
+		{
+			inputIsValid.call(this);
+		}
+		else
+		{
+			inputIsInvalid.call(this);
+		}
+	});
+
+	$('#pw1-register').on('focus', function() {
+		$(this).removeClass("invalid");
+	});
+
+
+	// register pw confirmation
+	$('#pw2-register').on('blur', function() {
+		var pw1 = $('#pw1-register').val();
+		var pw2 = $('#pw2-register').val();
+		
+		if( arePWsEqual(pw1, pw2) )
+		{
+			inputIsValid.call(this);
+		}
+		else
+		{
+			inputIsInvalid.call(this);
+		}
+	});
+
+})();
+
+
+
+
+
+
+
+
+
--- index.php
+++ index.php
@@ -9,18 +9,58 @@
 	<body>
 		<h4>Login or Register Below</h4>
 
-		<form action="">
-			<label for="un">Username</label>
-			<input id="un" name="un">
-			
-			<label for="pw">Password</label>
-			<input id="pw" name="pw">
+		<div id="content">
 
-			<input id="submit" name="submit" type="submit">
-		</form>
+			<div id="form-login-container">
+				<h3>Login</h3>
+				<form id="form-login" name="form-login" action="login.php" method="GET">
+					<label for="email-login">Email Address:</label>
+					<input id="email-login" name="email-login">
+					<p class="error">This is not a valid email address.</p>
+
+					<br>
+					
+					<label for="pw-login">Password:</label>
+					<input id="pw-login" name="pw-login">
+					<p class="error">Must contain at least: - 8 characters, - 1 number, - 1 capital letter</p>
+
+					<br>
+
+					<input id="submit-login" name="submit-login" type="submit" value="Login">
+				</form>
+			</div> <!-- #form-login-container -->
+
+			<div id="form-register-container">
+				<h3>Register</h3>
+				<form id="form-register" name="form-register" action="register.php" method="GET">
+					<label for="email-register">Email Address:</label>
+					<input id="email-register" name="email-register">
+					<p class="error">This is not a valid email address.</p>
+
+					<br>
+					
+					<label for="pw1-register">Password:</label>
+					<input id="pw1-register" name="pw1-register">
+					<!-- <p>Password must be at least 8 characters, containing at least 1 number and 1 capital letter.</p> -->
+					<p class="error">Must contain at least: - 8 characters, - 1 number, - 1 capital letter</p>
+
+					<br>
+
+					<label for="pw2-register">Confirm Password:</label>
+					<input id="pw2-register" name="pw2-register">
+					<p class="error">The passwords do not match.</p>
+
+					<br>
+
+					<input id="submit-register" name="submit-register" type="submit" value="Register">
+				</form>
+			</div> <!-- #form-register-container -->
+
+		</div> <!-- #content -->
+
 	</body>
 
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-	<script>//js will be inlcuded here</script>
+	<script src="includes/js/indexmin.js"></script>
 	<script src="http://localhost:35729/livereload.js"></script>
 </html>
